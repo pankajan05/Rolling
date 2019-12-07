79913b74b324e9bd3a04f2936f29a2f77fb85925
Pankajan Satkunam <pankajan05@gmail.com>
2019-12-07 20:58:20 +0530

    Add the picture for the rolling dice

--- app/src/main/java/com/kajan/rolling/MainActivity.kt
+++ app/src/main/java/com/kajan/rolling/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kajan.rolling
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,6 +25,18 @@ class MainActivity : AppCompatActivity() {
     private fun roll() {
         var roll_text: TextView = findViewById(R.id.roll_text)
         var random = Random.nextInt(6)+1
+
+        var drawable_Resource = when(random){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
         roll_text.text = random.toString()
+        var roll_img:ImageView = findViewById(R.id.dice_img)
+        roll_img.setImageResource(drawable_Resource)
     }
 }
